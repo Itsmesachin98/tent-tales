213ef7a916061bdcb828360ee8ca94ab43d34eb1
Sachin Kumar <sachinkumarskpog@gmail.com>
2025-08-06 23:23:04 +0530

    feat: add edit.ejs page to update existing campgrounds

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
@@ -14,6 +15,7 @@ db.once("open", () => {
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -36,9 +38,24 @@ app.get("/campgrounds/:id", async (req, res) => {
     res.render("campgrounds/show", { campground });
 });
 
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
+});
+
 app.post("/campgrounds", async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    const { title, location } = req.body.campground;
+    const campground = await Campground.findByIdAndUpdate(id, {
+        title,
+        location,
+    });
     res.redirect(`/campgrounds/${campground._id}`);
 });
 
