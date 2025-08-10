4cfc78b5aadad20258a2a2b6b94011a1298808fa
Sachin Kumar <sachinkumarskpog@gmail.com>
2025-08-10 14:24:48 +0530

    feat: add layouts/boilerplate.ejs for reusable page structure

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
+const ejsMate = require("ejs-mate");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
 
@@ -17,6 +18,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
