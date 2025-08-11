9169647fa93ab9ae955be6346879bcdc24b6cb58
Sachin Kumar <sachinkumarskpog@gmail.com>
2025-08-11 11:29:55 +0530

    chore: update seeds/index.js with revised seed data and logic

--- seeds/index.js
+++ seeds/index.js
@@ -16,9 +16,14 @@ async function seedDB() {
     await Campground.deleteMany({});
 
     for (let i = 0; i < descriptors.length; i++) {
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             title: `${places[i]}, ${descriptors[i]}`,
             location: `${cities[i].city}, ${cities[i].state}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            price,
+            description:
+                "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id minima fugiat dicta et natus tempore, placeat provident voluptates unde a facilis vitae nostrum quis quos accusamus ad pariatur quo doloribus.",
         });
 
         await camp.save();
