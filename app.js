7a73070b03afc186fe76a3a3108e3821a632e5c3
Sachin Kumar <sachinkumarskpog@gmail.com>
2025-10-13 18:22:40 +0530

    feat: add public folder to serve static files

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
